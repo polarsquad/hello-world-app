d729fe24621c181ccfdece42925c0c2e7c3b42d6
Erno Aapa <erno.aapa@gmail.com>
2018-10-24 14:28:52 +0300

    Replace logo with Jeremy

--- src/views.js
+++ src/views.js
@@ -5,13 +5,7 @@ import { template, sample } from 'lodash';
 import Router from 'express-promise-router';
 
 const IMAGES = [
-  { img: '/assets/ps_blackmetal.png' },
-  { img: '/assets/ps_lelukauppa.png' },
-  { img: '/assets/ps_mylittlepony.png' },
-  { img: '/assets/ps_santacruz.png' },
-  { img: '/assets/ps_sexpistols.png' },
-  { img: '/assets/ps_suicidesquad.png' },
-  { img: '/assets/ps_trasher.png' },
+  { img: '/assets/ps_jeremy.png' },
 ];
 
 const render = (view, ctx = {}) => template(readFileSync(resolve(__dirname, `views/${view}.html`)))(ctx);
