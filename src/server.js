31b37bd6738585b6b99aa6df5be3635ac0d881a9
Erno Aapa <erno.aapa@gmail.com>
2018-10-24 16:07:11 +0300

    Added possibility to demo secrets

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import { createServer } from 'http';
 
 import app from './app';
 
+if (process.env.MY_SECRET) {
+  console.log(`Uuuh.. you told me a secret (psst! It\s "${process.env.MY_SECRET}" ;) )`);
+}
+
 createServer(app).listen(3000, () => {
   console.log('Api Server listening on port http://localhost:3000');
 });
